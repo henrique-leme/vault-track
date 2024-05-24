a2215d436c44d997e1577c2a49c40ddddea6c8d6
henrique-leme <henriqueleme.brito@outlook.com>
2024-05-24 15:30:04 -0300

    feat: encrypted password

---
+++ app/server/src/utils/encryptedPassword.ts
@@ -0,0 +1,7 @@
+import * as bcrypt from 'bcrypt'
+
+export const hashPassword = async (password: string): Promise<string> => {
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  return hashedPassword
+}
