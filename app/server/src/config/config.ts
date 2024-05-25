dd8c4a4c7d8b94121031293dab32830903a07e42
henrique-leme <henriqueleme.brito@outlook.com>
2024-05-25 16:47:04 -0300

    fix: change port docker

--- app/server/src/config/config.ts
+++ app/server/src/config/config.ts
@@ -5,6 +5,7 @@ dotenv.config()
 const PROCESS_ENV = process.env
 
 export const config = {
+  MONGO_DB_NAME: PROCESS_ENV.MONGO_DB_NAME ?? 'vault-track',
   PORT: PROCESS_ENV.PORT ?? 8080,
-  MONGO_URI: PROCESS_ENV.MONGO_URI ?? 'mongodb://localhost:27017',
+  MONGO_URI: PROCESS_ENV.MONGO_URI ?? 'mongodb://localhost:37017',
 }
--- app/server/src/mongo.ts
+++ app/server/src/mongo.ts
@@ -3,9 +3,9 @@ import mongoose from 'mongoose'
 
 export async function mongo() {
   try {
-    await mongoose.connect(config.MONGO_URI)
-
-    console.log('MongoDB connection established')
+    await mongoose.connect(config.MONGO_URI, {
+      dbName: config.MONGO_DB_NAME,
+    })
   } catch (error) {
     console.error('Failed to connect to MongoDB:', error)
     throw error
