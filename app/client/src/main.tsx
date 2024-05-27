2e5a1c2e6ec9209d1f410c4bee08f92242bd4afb
henrique-leme <henriqueleme.brito@outlook.com>
2024-05-27 01:07:52 -0300

    feat: add shadcn

--- app/client/src/main.tsx
+++ app/client/src/main.tsx
@@ -1,14 +1,14 @@
-import { RelayEnvironmentProvider } from "react-relay";
-import { RelayEnvironment } from "./RelayEnvironment";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { RelayEnvironmentProvider } from 'react-relay'
+import { RelayEnvironment } from './RelayEnvironment'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './styles/index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <RelayEnvironmentProvider environment={RelayEnvironment}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </RelayEnvironmentProvider>
-);
+  </RelayEnvironmentProvider>,
+)
