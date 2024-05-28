84d806aa2a1c2ffa5594e6cf67d24fce625fc51d
henrique-leme <henriqueleme.brito@outlook.com>
2024-05-27 23:08:19 -0300

    feat: add relay + shadcnui

--- app/client/src/index.tsx
+++ app/client/src/index.tsx
@@ -1,9 +1,9 @@
 import { RelayEnvironmentProvider } from 'react-relay'
-import { RelayEnvironment } from './RelayEnvironment'
+import { RelayEnvironment } from './RelayEnvironment.ts'
+import '@/styles/global.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <RelayEnvironmentProvider environment={RelayEnvironment}>
