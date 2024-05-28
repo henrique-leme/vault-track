4b3c7f502bd16eb483cce597e07534e9e6791cfe
henrique-leme <henriqueleme.brito@outlook.com>
2024-05-27 23:09:50 -0300

    feat: BalanceCard

---
+++ app/client/src/components/BalanceCard.tsx
@@ -0,0 +1,16 @@
+import { Card, CardContent, CardHeader } from './ui/card'
+
+const BalanceCard = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <h2>Account Balance</h2>
+      </CardHeader>
+      <CardContent>
+        <p>12232131234</p>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default BalanceCard
