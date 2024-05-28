c4c550d85f2639a0bb4410cef3c65b43f6e0ab1c
henrique-leme <henriqueleme.brito@outlook.com>
2024-05-27 23:09:07 -0300

    feat: Login Page

---
+++ app/client/src/pages/LoginPage.tsx
@@ -0,0 +1,22 @@
+import { LoginForm } from '@/components/LoginForm'
+import { Link } from 'react-router-dom'
+
+function LoginPage() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to VaultTrack</h1>
+        <p>
+          Enjoy all the features that make it easy for you to manage your
+          finances
+        </p>
+        <LoginForm />
+        <p>
+          Don't have an account yet? <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default LoginPage
