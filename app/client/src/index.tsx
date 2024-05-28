d411b97f9af12ed5f79cdf3b7304588b4b8c1118
henrique-leme <henriqueleme.brito@outlook.com>
2024-05-28 15:51:31 -0300

    feat: global css

--- app/client/src/index.tsx
+++ app/client/src/index.tsx
@@ -1,9 +1,9 @@
 import { RelayEnvironmentProvider } from 'react-relay'
 import { RelayEnvironment } from './RelayEnvironment.ts'
-import '@/styles/global.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import '@/styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <RelayEnvironmentProvider environment={RelayEnvironment}>
