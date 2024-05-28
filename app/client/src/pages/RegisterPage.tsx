2c5127607d5e2d0da5aff3256d231291e7a3341a
henrique-leme <henriqueleme.brito@outlook.com>
2024-05-27 23:09:19 -0300

    feat: Register page

---
+++ app/client/src/pages/RegisterPage.tsx
@@ -0,0 +1,14 @@
+import { RegisterForm } from '@/components/RegisterForm'
+
+function RegisterPage() {
+  return (
+    <>
+      <div>
+        <h1>Register</h1>
+        <RegisterForm />
+      </div>
+    </>
+  )
+}
+
+export default RegisterPage
